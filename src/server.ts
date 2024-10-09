1d49eb3bb0c5e18542862bf17dfc85a904799f34
Pedro Henrique <hpedrobs@gmail.com>
2024-10-09 18:40:46 -0300

    Atualiza servidor em TypeScript para exibir o IP atual em vez de localhost

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // internal
 import '@config/env'
 import messageRoutes from '@routes/messageRoutes'
+import os from 'os'
 
 // external
 import express from 'express'
@@ -15,7 +16,21 @@ app.use(express.json())
 // Rotas
 app.use('/messages',  messageRoutes)
 
+// Função para obter o IP da máquina
+const getLocalIP = (): string => {
+    const interfaces = os.networkInterfaces()
+    for (const interfaceName in interfaces) {
+        for (const iface of interfaces[interfaceName]!) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address // Retorna o IP da máquina
+            }
+        }
+    }
+    return 'localhost' // Caso não encontre, retorna 'localhost'
+}
+
 // Iniciar o servidor
 app.listen(PORT, () => {
-    console.log(`O servidor está sendo executado em http://localhost:${PORT}`)
+    const ip = getLocalIP()
+    console.log(`O servidor está sendo executado em http://${ip}:${PORT}`)
 })
